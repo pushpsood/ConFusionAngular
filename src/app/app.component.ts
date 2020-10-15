fea215ee0737c021ef7f199b1d35cb33cabb2217
Pushp Sood <pushpsood27@gmail.com>
2020-10-15 23:14:09 +0530

    Angular Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  /* 
+  //inline template using back qoutes
+  //inline template not recommended but good for simple application
+  template: `<h1>{{tittle}}</h1>
+  //same for styles below too
+  `,
+  */
   styleUrls: ['./app.component.scss']
 })
+//below command will make import data to components
 export class AppComponent {
   title = 'conFusion';
 }
